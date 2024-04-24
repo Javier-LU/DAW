77f5dd31afc2de26382526c6cceb6e9bf4b0daa1
Javier-LU <javier_luque@yahoo.com>
2024-04-24 19:26:57 +0200

    vacio

---
+++ src/main/java/com/ESAD/ESAD/EsadApplication.java
@@ -0,0 +1,13 @@
+package com.ESAD.ESAD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ESAD/ESAD/EsadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ESAD.ESAD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
